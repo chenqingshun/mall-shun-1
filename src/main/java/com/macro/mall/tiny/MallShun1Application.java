b2fa8af8c820b2dcfa72fa94ba6621db80e334ce
陈清顺 <chenqingshun@yeah.net>
2021-12-27 21:28:00 +0800

    1.springboot+mybatis+数据库种种 2.mybatis generator自动代码 3.swagger的api文档注释 4.整合redis

---
+++ src/main/java/com/macro/mall/tiny/MallShun1Application.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallShun1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallShun1Application.class, args);
+    }
+
+}
